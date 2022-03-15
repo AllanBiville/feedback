acadb1e2140dd5bf18f9e3e99625af290ef93414
BIVILLE Allan <biviall@ndlp.fr>
2022-03-15 16:06:25 +0100

    codepin-templates

--- src/Controller/VisitorController.php
+++ src/Controller/VisitorController.php
@@ -5,45 +5,75 @@ namespace App\Controller;
 
 use App\Entity\Avis;
 use App\Form\AvisType;
+use App\Form\CodePinType;
 use App\Entity\AvisTypesCategories;
+use App\Repository\QrcodePinRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Repository\TypesCategoriesRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-class VisitorController extends AbstractController
-{
     /**
      * @Route("/visitor", name="app_visitor")
      */
-    public function index(TypesCategoriesRepository $typesCategoriesRepository, Request $request, EntityManagerInterface $entityManager): Response
+class VisitorController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_visitor")
+     */
+    public function codePin(RequestStack $requestStack,QrcodePinRepository $pinRepository, Request $request): Response
     {
-        $avis = new Avis();
-        
-        $form = $this->createForm(AvisType::class, $avis);
-        $form->handleRequest($request);
-        if ($form->isSubmitted() && $form->isValid()) {
-            foreach ($typesCategoriesRepository->findAll() as $tc){
-                $note = $request->get($tc->getId());
-                $atc = new AvisTypesCategories();
-                $atc->setAvis($avis);
-                $atc->setTypesCategories($tc);
-                $atc->setNote($note);
-                $entityManager->persist($atc);
+            $form = $this->createForm(CodePinType::class);
+            $form->handlerequest($request);
+    
+            if ($form->isSubmitted() && $form->isValid()){
+                $value= $request->get('pin_qr_code');
+                dump($value);
+                $pin = $value['pin'];
+                
+                // $session = $requestStack->getSession();
+                // $session->set('pinOK', 'pinOK');
+                // $this->addFlash('danger', 'Mauvais code pin !');
+    
+                // if ($pinRepository->searchPin($pin) != null){
+                //     return $this->redirectToRoute("qrcode_visitor");
+                // }
             }
-
-            $entityManager->persist($avis);
-
-            $entityManager->flush();
-
-            // return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
-        }
-        return $this->render('visitor/index.html.twig', [
-            'controller_name' => 'VisitorController',
-            'categories' => $typesCategoriesRepository->findAll(),
+        return $this->render('visitor/codepin.html.twig', [
             'form' => $form->createView(),
+
         ]);
     }
+
+
+    // public function form(TypesCategoriesRepository $typesCategoriesRepository, Request $request, EntityManagerInterface $entityManager): Response
+    // {
+    //     $avis = new Avis();
+        
+    //     $form = $this->createForm(AvisType::class, $avis);
+    //     $form->handleRequest($request);
+    //     if ($form->isSubmitted() && $form->isValid()) {
+    //         foreach ($typesCategoriesRepository->findAll() as $tc){
+    //             $note = $request->get($tc->getId());
+    //             $atc = new AvisTypesCategories();
+    //             $atc->setAvis($avis);
+    //             $atc->setTypesCategories($tc);
+    //             $atc->setNote($note);
+    //             $entityManager->persist($atc);
+    //         }
+
+    //         $entityManager->persist($avis);
+
+    //         $entityManager->flush();
+
+    //         // return $this->redirectToRoute('user_index', [], Response::HTTP_SEE_OTHER);
+    //     }
+    //     return $this->render('visitor/index.html.twig', [
+    //         'controller_name' => 'VisitorController',
+    //         'categories' => $typesCategoriesRepository->findAll(),
+    //         'form' => $form->createView(),
+    //     ]);
+    // }
 }
