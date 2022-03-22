8553a9d64234172665f4de37db6f1354adaa5fc5
BIVILLE Allan <biviall@ndlp.fr>
2022-03-22 14:24:30 +0100

    user_crud OK

--- src/Form/QrcodePinType.php
+++ src/Form/QrcodePinType.php
@@ -7,13 +7,14 @@ use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 
 class QrcodePinType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('pin',IntegerType::class)
+            ->add('pin',PasswordType::class)
         ;
     }
 
