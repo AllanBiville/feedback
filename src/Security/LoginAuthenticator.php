9c8d629708186543a3c3d09029b6ecada2403f70
Allan BIVILLE <71440994+AllanBiville@users.noreply.github.com>
2022-04-28 10:42:35 +0200

    Update LoginAuthenticator.php

--- src/Security/LoginAuthenticator.php
+++ src/Security/LoginAuthenticator.php
@@ -21,7 +21,7 @@ class LoginAuthenticator extends AbstractLoginFormAuthenticator
 
     public const LOGIN_ROUTE = 'app_login';
 
-    private UrlGeneratorInterface $urlGenerator;
+    private $urlGenerator;
 
     public function __construct(UrlGeneratorInterface $urlGenerator)
     {
